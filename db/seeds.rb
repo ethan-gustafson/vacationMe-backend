572facfd11cc069cc1434552d1647e8edf6b1325
Ethan <ethan_gustafson@outlook.com>
2020-04-16 22:00:43 -0400

    added new User seeds using Faker Gem, added has_secure_password to User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+    name: "Ethan Gustafson",
+    username: "GoodGuyGuf",
+    password: "123",
+    email: "ethan_gustafson@outlook.com"
+)
+
+20.times do
+    User.create(
+        name: Faker::Name.name,
+        username: Faker::Internet.username,
+        password: Faker::Internet.password,
+        email: Faker::Internet.safe_email
+    )
+end
