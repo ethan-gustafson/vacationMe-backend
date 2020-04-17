19b724a52140ff3e7262c249a2d5900e59ef9941
Ethan <ethan_gustafson@outlook.com>
2020-04-17 13:03:29 -0400

    migrated posts table, created index & show action in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+
+    def index
+        posts = Post.all
+        render json: PostSerializer.new(posts)
+    end
+
+    def show
+        post = Post.find_by(id: params[:id])
+        if post
+            render json: PostSerializer.new(post)
+        else
+            render json: {message: "No Post Found."}
+        end
+    end
 end
