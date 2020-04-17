6bd34c174fcc06afa50730c2b4be4cdab6308865
Ethan <ethan_gustafson@outlook.com>
2020-04-16 22:40:21 -0400

    added login route in UsersController & config/routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,14 @@ class UsersController < ApplicationController
         users = User.all
         render json: UserSerializer.new(users)
     end
-    
+
+    def login
+        user = User.find_by(username: params[:user][:username])
+        if user.try(:authenticate, params[:password])
+            render json: UserSerializer.new(user)
+        else
+            render json: {message: "No User Found."}
+        end
+    end
+
 end
