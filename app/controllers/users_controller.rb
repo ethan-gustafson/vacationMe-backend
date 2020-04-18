9128e4dc11bb19880b95312aee3fb95cfeb7e940
Ethan <ethan_gustafson@outlook.com>
2020-04-18 02:22:38 -0400

    added functional UsersController create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,20 @@ class UsersController < ApplicationController
         render json: UserSerializer.new(users)
     end
 
+    def create
+        user = User.create(
+            name: params[:user][:name],
+            username: params[:user][:username],
+            email: params[:user][:email],
+            password: params[:password]
+        )
+        if user
+            render json: UserSerializer.new(user)
+        else
+            render json: {message: "Incorrect fields"}
+        end
+    end
+
     def login
         user = User.find_by(email: params[:user][:email])
         if user.try(:authenticate, params[:password])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
-  resources :users, only: [:index]
+  resources :users, only: [:index, :create]
   post 'login', to: 'users#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
