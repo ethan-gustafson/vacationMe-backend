38e6847d143daf4f5fb87652bd2021acfab02104
Ethan <ethan_gustafson@outlook.com>
2020-04-18 16:29:01 -0400

    Added simple validations to user.rb/post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
     belongs_to :user
+    validates :title, :location, :caption, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :posts
+    validates :name, :username, :email, :password, presence: true
+    validates :username, :email, uniqueness: true
 end
