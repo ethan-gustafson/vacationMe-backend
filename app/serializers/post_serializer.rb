cc31f2f9f630080d2b6971f545eab7a69125c8a8
Ethan <ethan_gustafson@outlook.com>
2020-04-19 01:34:04 -0400

    added :created_at & :updated_at json for users/posts

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,4 @@
 class PostSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :location, :title, :caption, :user_id
+  attributes :location, :title, :caption, :user_id, :created_at, :updated_at
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :username, :email
+  attributes :name, :username, :email, :created_at, :updated_at
 end
