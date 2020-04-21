c9d9483f24c4c079036753a33a2b75c9fb8eddc4
Ethan <ethan_gustafson@outlook.com>
2020-04-21 03:02:54 -0400

    removed has_many: :through relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :posts
     has_many :likes
-    has_many :liked_posts, through: :posts, source: :likes # User has many posts they have liked: "liked_posts" - so we want to be able to call User.all.first.liked_posts
 
     validates :name, :username, :email, :password, presence: true
     validates :username, :email, uniqueness: true
