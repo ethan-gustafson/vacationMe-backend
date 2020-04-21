2ab5119809cb0f40e065594fea556615174fb4c2
Ethan <ethan_gustafson@outlook.com>
2020-04-21 03:49:35 -0400

    added has_many through relationships between users, posts, likes & comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
     has_secure_password
     has_many :posts
     has_many :likes
+    has_many :liked_posts, through: :likes, source: :post # returns the post instance, not the like instance
     has_many :comments
+    has_many :commented_posts, through: :comments, source: :post # returns the post instance, not the comment instance
 
     validates :name, :username, :email, :password, presence: true
     validates :username, :email, uniqueness: true
