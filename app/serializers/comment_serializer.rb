f75a2647c39699b0a85040ec39a83bab34b57dc5
Ethan <ethan_gustafson@outlook.com>
2020-04-21 20:13:57 -0400

    added a display_user method to comment class

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,4 @@
 class CommentSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :description, :user_id, :post_id, :created_at, :updated_at
+  attributes :description, :user_id, :post_id, :created_at, :updated_at, :display_user
 end
