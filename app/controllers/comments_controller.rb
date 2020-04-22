19bf7a033dffadca8c8aee3de7befd2de6528f23
Ethan <ethan_gustafson@outlook.com>
2020-04-21 20:01:17 -0400

    created comments create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,16 @@ class CommentsController < ApplicationController
     end
 
     def create
-
+        comment = Comment.create(
+            description: params[:comment][:description], 
+            post_id: params[:comment][:post_id], 
+            user_id: params[:comment][:user_id]
+        )
+        if comment
+            render json: CommentSerializer.new(comment)
+        else
+            render json: {message: "Unable to save."}
+        end
     end
 
     def update
