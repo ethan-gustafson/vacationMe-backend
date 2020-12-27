acf4beb67e1384c92cec5756f98fa05fd35c4459
Ethan <ethan_gustafson@outlook.com>
2020-12-27 15:45:52 -0500

    remove login action, format indentation, add user_params private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,21 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: UserSerializer.new(users)
+  end
 
-    def index
-        users = User.all
-        render json: UserSerializer.new(users)
+  def create
+    user = User.create(user_params)
+    if user
+      render json: UserSerializer.new(user)
+    else
+      render json: { message: "Incorrect fields" }
     end
+  end
 
-    def create
-        user = User.create(
-            name: params[:user][:name],
-            username: params[:user][:username],
-            email: params[:user][:email],
-            password: params[:password]
-        )
-        if user
-            render json: UserSerializer.new(user)
-        else
-            render json: {message: "Incorrect fields"}
-        end
-    end
+  private
 
-    def login
-        user = User.find_by(email: params[:user][:email])
-        if user.try(:authenticate, params[:password])
-            render json: UserSerializer.new(user)
-        else
-            render json: {message: "No User Found."}
-        end
-    end
-
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :password)
+  end
 end
