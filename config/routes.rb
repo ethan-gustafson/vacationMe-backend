7b9c8ebf7caf54629928d8008196e0e20ffa5a98
Ethan <ethan_gustafson@outlook.com>
2020-12-27 15:46:31 -0500

    add sessions create route in config/routes.rb, remove user login route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :likes, only: [:index, :create, :update]
   resources :posts, only: [:index, :show, :create, :update, :destroy]
   resources :users, only: [:index, :create]
-  post 'login', to: 'users#login'
+  resources :sessions, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
